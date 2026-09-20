181bf35ae62df82ce20f724b6d1b43640fc19f7c
rayvooficial-arch <rayvooficial@gmail.com>
2026-09-20 10:50:12 -0400

    refactor: shorten prelaunch page journey

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import React, { useState } from 'react';
 import { Navbar } from './components/Navbar';
 import { Hero } from './components/Hero';
-import { ConceptSection } from './components/ConceptSection';
 import { ExperiencesSection } from './components/ExperiencesSection';
-import { ExperienceMatcher } from './components/ExperienceMatcher';
 import { AccommodationsSection } from './components/AccommodationsSection';
 import { AccommodationDetailModal } from './components/AccommodationDetailModal';
 import { StayModesSection } from './components/StayModesSection';
-import { KitchenAndPantrySection } from './components/KitchenAndPantrySection';
 import { PersonalizeExperienceSection } from './components/PersonalizeExperienceSection';
 import { PricingSection } from './components/PricingSection';
-import { SocialProofSection } from './components/SocialProofSection';
 import { FaqSection } from './components/FaqSection';
 import { InfoSection } from './components/InfoSection';
 import { LocationSection } from './components/LocationSection';
@@ -27,38 +23,22 @@ export default function App() {
     openBookingEngine({ accommodationId });
   };
 
-  const handleOpenDetailModal = (acc: Accommodation) => {
-    setSelectedAccForDetail(acc);
-  };
-
-  const handleCloseDetailModal = () => {
-    setSelectedAccForDetail(null);
-  };
-
   return (
     <div className="min-h-screen bg-[#FAF7F2] text-[#2C332D] flex flex-col selection:bg-[#1c3224] selection:text-[#FAF7F2]">
       <Navbar onOpenBooking={handleOpenBooking} />
 
       <main className="flex-1">
-        <Hero
-          onOpenBooking={() => handleOpenBooking()}
-          videoUrl="/SaveClip.mp4"
-        />
-
-        <ConceptSection />
-        <ExperienceMatcher onSelectAccommodation={handleOpenBooking} />
+        <Hero onOpenBooking={() => handleOpenBooking()} videoUrl="/SaveClip.mp4" />
 
         <AccommodationsSection
           onOpenBooking={handleOpenBooking}
-          onOpenDetails={handleOpenDetailModal}
+          onOpenDetails={setSelectedAccForDetail}
         />
 
         <StayModesSection onOpenBooking={handleOpenBooking} />
         <ExperiencesSection onOpenBooking={() => handleOpenBooking()} />
-        <KitchenAndPantrySection />
         <PersonalizeExperienceSection />
         <PricingSection onOpenBooking={handleOpenBooking} />
-        <SocialProofSection />
         <FaqSection />
         <InfoSection />
         <LocationSection />
@@ -69,9 +49,9 @@ export default function App() {
 
       <AccommodationDetailModal
         accommodation={selectedAccForDetail}
-        onClose={handleCloseDetailModal}
+        onClose={() => setSelectedAccForDetail(null)}
         onOpenBooking={(accId) => {
-          handleCloseDetailModal();
+          setSelectedAccForDetail(null);
           handleOpenBooking(accId);
         }}
       />
